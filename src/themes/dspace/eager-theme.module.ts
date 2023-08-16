9e7b5e5442bae2f73b1d2567849773e1ff78f218
john-zhang1 <john-zhang1@hotmail.com>
2023-08-16 12:57:36 -0500

    update eager-theme.module file

--- src/themes/dspace/eager-theme.module.ts
+++ src/themes/dspace/eager-theme.module.ts
@@ -9,12 +9,22 @@ import { RootModule } from '../../app/root.module';
 import { NavbarModule } from '../../app/navbar/navbar.module';
 import { SharedBrowseByModule } from '../../app/shared/browse-by/shared-browse-by.module';
 import { ResultsBackButtonModule } from '../../app/shared/results-back-button/results-back-button.module';
+import { ItemPageModule } from '../../app/item-page/item-page.module';
+import { UntypedItemComponent } from './app/item-page/simple/item-types/untyped-item/untyped-item.component';
+import { ItemSharedModule } from '../../app/item-page/item-shared.module';
+import { AltmetricBadgeComponent } from './app/item-page/simple/item-types/untyped-item/altmetric-badge/altmetric-badge.component';
+import { ItemPageCclicenseFieldComponent } from './app/item-page/simple/item-types/untyped-item/cclicense-field/item-page-cclicense-field.component';
+import { DsoPageModule } from "../../app/shared/dso-page/dso-page.module";
 
 /**
  * Add components that use a custom decorator to ENTRY_COMPONENTS as well as DECLARATIONS.
  * This will ensure that decorator gets picked up when the app loads
  */
-const ENTRY_COMPONENTS = [];
+const ENTRY_COMPONENTS = [
+  UntypedItemComponent,
+  AltmetricBadgeComponent,
+  ItemPageCclicenseFieldComponent,
+];
 
 const DECLARATIONS = [
   ...ENTRY_COMPONENTS,
@@ -32,6 +42,9 @@ const DECLARATIONS = [
     ResultsBackButtonModule,
     RootModule,
     NavbarModule,
+    ItemPageModule,
+    ItemSharedModule,
+    DsoPageModule,
   ],
   declarations: DECLARATIONS,
   providers: [
