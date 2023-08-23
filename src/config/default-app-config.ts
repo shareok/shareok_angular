cc449e067b1e05bd4b4c9f54942a2ab2575d685a
john-zhang1 <john-zhang1@hotmail.com>
2023-08-23 14:12:40 -0500

    show access statuses

--- src/config/default-app-config.ts
+++ src/config/default-app-config.ts
@@ -278,7 +278,7 @@ export class DefaultAppConfig implements AppConfig {
       undoTimeout: 10000 // 10 seconds
     },
     // Show the item access status label in items lists
-    showAccessStatuses: false,
+    showAccessStatuses: true,
     bitstream: {
       // Number of entries in the bitstream list in the item view page.
       // Rounded to the nearest size in the list of selectable sizes on the
